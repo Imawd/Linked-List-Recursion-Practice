4157c55faa7dac6172d3c70fc86c81429fd359b1
Imaad Feerasta <feerasta@usc.edu>
2022-09-11 23:12:00 +0000

    Check in changes for problem 2

--- ulliststr.cpp
+++ ulliststr.cpp
@@ -2,6 +2,8 @@
 #include <stdexcept>
 #include "ulliststr.h"
 
+using namespace std;
+
 ULListStr::ULListStr()
 {
   head_ = NULL;
@@ -25,6 +27,174 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+void ULListStr::push_back(const std::string& val)
+{ 
+	if (tail_ == NULL)
+  {
+    tail_ = new Item();
+    tail_->last = 1;
+    tail_->first = 0;
+    tail_->val[tail_->first] = val;
+    head_ = tail_;
+    size_++;
+  }
+
+  else
+  {
+
+    if (tail_->last != ARRSIZE)
+    {
+      tail_->val[tail_->last] = val;
+      tail_->last += 1;
+      size_++;
+    }
+
+    else 
+    {
+			Item* temp = new Item(); //not actually a temp, rather a new node, not to be dealloc'd
+			temp->val[0] = val;
+      temp->first = 0;
+			temp->last = 1;
+			temp->prev = tail_;
+			tail_->next = temp;
+			tail_ = temp;
+      size_++;
+    }
+    
+  }
+
+	
+
+}
+
+void ULListStr::pop_back()
+{
+
+  if (tail_ == NULL) return;
+
+  else
+  {
+    tail_->last -= 1;
+		size_ -= 1;
+
+    if (tail_->last - tail_->first == 0)
+    {
+        Item* temp = tail_;
+        tail_ = temp->prev;
+        if (tail_ != NULL) tail_->next = NULL;
+    		else head_ = NULL;
+
+        delete temp;
+      
+    }
+
+      
+  }
+
+
+}
+
+void ULListStr::push_front(const std::string& val)
+{
+	
+  if (head_ == NULL)
+  {
+    head_ = new Item();
+    size_++;
+		head_->val[ARRSIZE-1] = val;
+		head_->first = ARRSIZE-1;
+    head_->last = ARRSIZE;
+    tail_ = head_;
+  }
+
+  else 
+  {
+    if (head_->first != 0)
+    {
+      head_->first -= 1;
+      head_->val[head_->first] = val;
+      size_++;
+    }
+    else
+    {
+			Item* temp = new Item(); //not actually a temp, a new node and shouldn't be deallocated
+			temp->val[ARRSIZE - 1] = val;
+			temp->last = ARRSIZE;
+			temp->first = ARRSIZE-1;
+			temp->next = head_;
+			head_->prev = temp;
+			head_ = temp;
+			size_++;
+    }
+
+  }	
+		
+}
+
+void ULListStr::pop_front() 
+{
+  if (head_ == NULL) return;
+
+  else if (head_ != NULL)
+  {
+    size_--;
+    head_->first += 1;
+
+    if (head_->last - head_->first == 0)
+    {
+      
+      Item* temp = head_;
+      head_ = temp->next;
+      if (head_ != NULL) head_->prev = NULL;
+    	else tail_ = NULL;
+      
+			delete temp;
+
+    }
+
+  }
+
+
+}
+
+std::string const & ULListStr::back() const 
+{
+  return tail_->val[tail_->last - 1];
+}
+
+std::string const & ULListStr::front() const
+{
+  return head_->val[head_->first];
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const
+{
+
+  if (head_ == nullptr) return NULL;
+
+  else if (loc > size_) return NULL;
+
+  else
+  {
+    Item* temp = head_;
+
+    while (loc >= 0)
+    {
+      
+      if ((temp->last - temp->first) >= loc)
+      {     
+        return &temp->val[temp->first + loc];
+      }      
+      
+      loc -= (temp->last - temp->first);
+      temp = temp->next;
+
+    }
+    delete temp;
+  }
+
+  return NULL;
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
--- ulliststr_test.cpp
+++ ulliststr_test.cpp
@@ -1,11 +1,196 @@
 /* Write your test code for the ULListStr in this file */
-
 #include <iostream>
+#include <cstddef>
+#include <stdexcept>
+#include "ulliststr.h"
 using namespace std;
 
 
 int main(int argc, char* argv[])
 {
+  ULListStr test1; //test push_back
+
+
+  test1.push_back("U"); //test empty
+
+	std::cout << test1.get(0) << endl;
+	std::cout << "Testing push_back() with one item" << endl;
+	if(test1.get(0) == "U") cout << "Test passed!" << endl << endl;
+
+  test1.push_back("S"); //test multiple items
+  test1.push_back("C");
+  test1.push_back("-");
+  test1.push_back("T");
+  test1.push_back("R");
+  test1.push_back("O");
+  test1.push_back("J");
+  test1.push_back("A");
+	test1.push_back("N");
+  
+
+
+	for (size_t i = 0; i < 10; i++)
+	{
+		std::cout << test1.get(i);
+	}
+
+	for (int i = 0; i < 5; i++) //test pop_front with multiple items
+	{
+		test1.pop_front();
+	}
+
+	std::cout << endl << "Testing pop_front() with multiple items: popping 5 chars from front expecting: 'R O J A N' " << endl;
+	
+	for (size_t i = 0; i < 5; i++)
+	{
+		std::cout << test1.get(i) << " ";
+	}
+
+	cout << endl << endl;
+
+
+	std::cout << "Testing pop_front() with one item" << endl;
+
+	for (int i = 0; i < 4; i++) //test pop_front with one item
+	{
+		test1.pop_front();
+	}
+
+	std::cout << test1.get(0) << endl;
+
+	test1.pop_front();
+
+	if (test1.size() == 0) std::cout << "Test passed!" << endl << endl;
+
+	test1.clear();
+
+
+
+	std::cout << "Testing pop_back() with one item" << endl;
+
+	test1.push_back("U");
+	test1.pop_back();
+
+	if (test1.size() == 0) std::cout << "Test passed!" << endl << endl;
+
+	test1.clear();
+
+	std::cout << "Testing pop_back() with multiple items" << endl;
+
+	test1.push_back("U");
+	test1.push_front("S"); 
+  test1.push_back("C");
+
+	for (int i = 0; i < 3; i++)
+	{
+		test1.pop_back();
+	}
+
+
+	if (test1.size() == 0) std::cout << "Test passed!" << endl << endl;
+
+	test1.clear();
+
+	ULListStr test2;	//test push_front
+
+	std::cout << "Testing push_front with empty list" << endl;
+
+	test2.push_front("A"); //when list is empty
+	std::cout << test2.get(0) << endl;
+	if (test2.get(0) == "A") std::cout << "Test passed!" << endl << endl;
+
+	test2.push_front("R"); //multiple items push_front
+	test2.push_front("E");
+	test2.push_front("-");
+	test2.push_front("W");
+	test2.push_front("E");
+	test2.push_front("-");
+	test2.push_front("T");
+	test2.push_front("H");
+	test2.push_front("E");
+	test2.push_front("R");
+	test2.push_front("E");
+	test2.push_front("-");
+	test2.push_front("Y");
+	test2.push_front("E");
+	test2.push_front("T");
+
+	std::cout << test2.get(15) << test2.get(14) << test2.get(13) << endl;
+
+	if(test2.get(15) == "A" && test2.get(14) == "R" && test2.get(13) == "E") cout << "Test passed!" << endl << endl;
+
+	test2.clear();
+
+	ULListStr test3;	//test push_front and push_back combo
+
+	std::cout << "Testing push_back() and push_front() combo" << endl;
+
+	test3.push_back("U");
+	test3.push_front("S"); 
+  test3.push_back("C");
+	test3.push_front("Y");
+	test3.push_front("E");
+	test3.push_front("T");
+	test3.push_back("J");
+  test3.push_back("A");
+	test3.push_back("N");
+	
+	std::cout << test3.get(0) << test3.get(1) << test3.get(2) << test3.get(3) << endl;
+
+	if (test3.get(0) == "T" && test3.get(1) == "E" && test3.get(2) == "Y" && test3.get(3) == "S") 
+	{
+		std::cout << "Test passed!" << endl;
+	}
+
+	std::cout << test3.get(5) << test3.get(6) << test3.get(7) << test3.get(8) << endl;
+
+
+	if (test3.get(5) == "C" && test3.get(6) == "J" && test3.get(7) == "A" && test3.get(8) == "N") 
+	{
+		std::cout << "Test passed!" << endl << endl;
+	}
+
+	std::cout << "Testing back()" << endl;
+
+	std::cout << test3.back() << " should be the same as " << test3.get(8) << endl;
+
+	if (test3.back() == test3.get(8)) std::cout << "Test passed!" << endl << endl;
+
+	std::cout << "Testing back() in succession" << endl;
+
+	int count = 0;
+
+	for (int i = 0; i < 3; i++)
+	{
+		if (test3.size() != 0)
+		{
+			test3.pop_back();
+			std::cout << test3.back() << " should be the same as " << test3.get(test3.size() - 1) << endl;
+			if (test3.back() == test3.get(test3.size() - 1)) count++;
+		}
+	}
+	if (count == 3) std::cout << "Test passed!" << endl << endl;
+
+	std::cout << "Testing front()" << endl;
+	std::cout << test3.front() << " should be the same as " << test3.get(0) << endl;
+	if (test3.front() == test3.get(0)) std::cout << "Test passed!" << endl << endl;
+
+	std::cout << "Testing front() in succession" << endl;
+
+	count = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		if (test3.size() != 0)
+		{
+			test3.pop_front();
+			std::cout << test3.front() << " should be the same as " << test3.get(0) << endl;
+			if (test3.front() == test3.get(0)) count++;
+		}
+	}
+
+	if (count == 3) std::cout << "Test passed!" << endl << endl;
+
+	
 
   return 0;
 }
